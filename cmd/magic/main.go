8ac6a157216a623402ce93c3a40282d37cb7168c
Jan Fooken <git@bode.fun>
2023-09-12 10:00:19 +0200

    Add flags and usage

--- cmd/magic/main.go
+++ cmd/magic/main.go
@@ -23,6 +23,15 @@ func main() {
 
 func mainE() error {
 	cli := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	cli.Usage = Usage(cli)
+
+	var includeGit bool
+
+	cli.BoolVar(&includeGit, "git", false, "Include files located in a \".git\" folder.")
+
+	var includeRust bool
+
+	cli.BoolVar(&includeRust, "rust", false, "Include files ending on the \".rs\" extension (temporary flag)")
 
 	if err := cli.Parse(os.Args[1:]); err != nil {
 		return err
@@ -51,7 +60,9 @@ func mainE() error {
 
 		isGitSubfolder := strings.Contains(path, ".git")
 
-		if isExe && !isGitSubfolder {
+		isRustFile := filepath.Ext(path) == ".rs"
+
+		if isExe && (!isGitSubfolder || includeGit) && (!isRustFile || includeRust) {
 			fmt.Fprintln(os.Stdout, path)
 		}
 
@@ -62,4 +73,11 @@ func mainE() error {
 	}
 
 	return nil
+}
+
+func Usage(f *flag.FlagSet) func() {
+	return func() {
+		fmt.Fprintf(f.Output(), "Usage of %s: path\n", f.Name())
+		f.PrintDefaults()
+	}
 }
